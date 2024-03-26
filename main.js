6adc1d46550c50d1e93e47951358386c881b02b2
Mohamed Ahmed <ma07041705@gmail.com>
2024-03-26 18:17:00 +0300

    Building the Simple Liker App

--- main.js
+++ main.js
@@ -3,6 +3,29 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", function() {
+  const errorModal = document.getElementById("modal");
+  errorModal.classList.add("hidden");
+
+  const likeButtons = document.querySelectorAll(".like");
+  likeButtons.forEach(button => {
+      button.addEventListener("click", function() {
+          mimicServerCall()
+              .then(() => {
+                  button.querySelector(".like-glyph").classList.add("activated-heart");
+                  button.querySelector(".like-glyph").innerHTML = "&#x2665;";
+              })
+              .catch(() => {
+                  errorModal.classList.remove("hidden");
+                  const modalMessage = document.getElementById("modal-message");
+                  modalMessage.textContent = "Server error. Please try again later.";
+                  setTimeout(() => {
+                      errorModal.classList.add("hidden");
+                  }, 3000);
+              });
+      });
+  });
+});
 
 
 
